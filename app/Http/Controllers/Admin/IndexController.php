5aef11e5486a8eb2b72683b3592b0c1eeab78c20
Prayer <prayer@crucis.cn>
2018-08-21 14:25:34 +0800

    'change_line_separator'

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -1,1 +1,34 @@
-<?phpnamespace App\Http\Controllers\Admin;use App\Http\Models\AdminRole;use Illuminate\Http\Request;class IndexController extends CommonController{    public function __construct()    {        parent::__construct();    }    public function logout(Request $request)    {        $request->session()->forget('admin_info');        return redirect('Admin/Login/index');    }    public function index()    {        $data = ['message_list'=>$this->message_list];        return view('Admin.Index.index',$data);    }    public function about_us()    {        return abort(404);    }    public function billboard()    {        return view('Admin.Index.billboard');    }}
+<?php
+namespace App\Http\Controllers\Admin;
+use App\Http\Models\AdminRole;
+use Illuminate\Http\Request;
+class IndexController extends CommonController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->forget('admin_info');
+        return redirect('Admin/Login/index');
+    }
+
+    public function index()
+    {
+        $data = ['message_list'=>$this->message_list];
+        return view('Admin.Index.index',$data);
+    }
+
+    public function about_us()
+    {
+        return abort(404);
+    }
+
+    public function billboard()
+    {
+        return view('Admin.Index.billboard');
+    }
+}
--- app/Http/Services/AdminService.php
+++ app/Http/Services/AdminService.php
@@ -1,1 +1,34 @@
-<?phpnamespace App\Http\Services;use App\Http\Repositories\AdminRepository;use Illuminate\Http\Request;use Illuminate\Support\Facades\Crypt;class AdminService{    protected $repository;    public function __construct(AdminRepository $adminRepository)    {        $this->repository = $adminRepository;    }    public function get()    {            }    public function edit($id,Request $request,QiniuService $qiniuService)    {        $data = $request->only('nickname','email','password','position','status');        $data['avatar'] = $qiniuService->upload($request->file('avatar'),'');        if ($data['password'])        {            $data['password'] = Crypt::encrypt($data['password']);        }else {            unset($data['password']);        }        $data['status'] = $data['status'] == 'on' ? 1 : 0;        return $this->repository->editAdminData($id,$data);    }}
+<?php
+namespace App\Http\Services;
+use App\Http\Repositories\AdminRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class AdminService
+{
+    protected $repository;
+
+    public function __construct(AdminRepository $adminRepository)
+    {
+        $this->repository = $adminRepository;
+    }
+
+    public function get()
+    {
+        
+    }
+
+    public function edit($id,Request $request,QiniuService $qiniuService)
+    {
+        $data = $request->only('nickname','email','password','position','status');
+        $data['avatar'] = $qiniuService->upload($request->file('avatar'),'');
+        if ($data['password'])
+        {
+            $data['password'] = Crypt::encrypt($data['password']);
+        }else {
+            unset($data['password']);
+        }
+        $data['status'] = $data['status'] == 'on' ? 1 : 0;
+        return $this->repository->editAdminData($id,$data);
+    }
+}
